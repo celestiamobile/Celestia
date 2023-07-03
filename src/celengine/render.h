c0537def47e82a7abaaf325f185592fe0a6efc0b
Levin Li <lilinfeng303@outlook.com>
2023-07-03 20:34:17 +0800

    Add back APIs, add support for 3D touches
    Add API for handling scaling and dragging in 3D

--- src/celengine/render.h
+++ src/celengine/render.h
@@ -141,6 +141,8 @@ class Renderer
 
     void setFaintestAM45deg(float);
     float getFaintestAM45deg() const;
+    void setMixedImmersion(bool);
+    bool getMixedImmersion() const;
     void setRTL(bool);
     bool isRTL() const;
 
@@ -590,6 +592,7 @@ class Renderer
  private:
     std::unique_ptr<ShaderManager> shaderManager{ std::make_unique<ShaderManager>() };
 
+    bool mixedImmersion{ false };
     int windowWidth{ 0 };
     int windowHeight{ 0 };
     float fov{ celestia::engine::standardFOV };
